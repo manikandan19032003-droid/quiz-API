ec418cb3a6170d3f7d3b4f6f3b080676a6236bf9
manikandan19032003-droid <manikandan19032003@gmail.com>
2026-01-23 10:23:41 +0530

    Update and rename mail.py to main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ genai.configure(api_key=api_key)
 
 # 2. MODEL: Use a valid model name (e.g., gemini-1.5-flash)
 # Note: 'files/...' references expire after 48 hours usually. Ensure the file exists.
-FILE_ID = "files/fzfborpiwvwg" 
+FILE_ID = "files/rigkhqqomqr9" 
 
 # @app.route('/generate_quiz', methods=['POST'])
 @functions_framework.http
@@ -26,7 +26,7 @@ def generate_quiz(request):
     # unit = data.get('unit')
     num = data.get('num')
 
-    if not subject or not unit or not num:
+    if not num:
         return jsonify({'error': 'Missing required fields'}), 400
 
     try:
